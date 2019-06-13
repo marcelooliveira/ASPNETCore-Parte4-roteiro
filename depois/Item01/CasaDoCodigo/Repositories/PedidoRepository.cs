1e373825150a877b852ae64c66e4a67389b441f0
marcelooliveira <mclricardo@gmail.com>
2019-06-13 14:11:02 -0300

    Relatório

--- depois/Item01/CasaDoCodigo/Repositories/PedidoRepository.cs
+++ depois/Item01/CasaDoCodigo/Repositories/PedidoRepository.cs
@@ -130,6 +130,7 @@ namespace CasaDoCodigo.Repositories
             var pedido = await GetPedidoAsync();
             await cadastroRepository.UpdateAsync(pedido.Cadastro.Id, cadastro);
             httpHelper.ResetPedidoId();
+            await GerarRelatorio(pedido);
             return pedido;
         }
 
@@ -145,6 +146,54 @@ namespace CasaDoCodigo.Repositories
         {
             contexto.Set<ItemPedido>()
                 .Remove(await GetItemPedidoAsync(itemPedidoId));
+        }
+
+        private async Task GerarRelatorio(Pedido pedido)
+        {
+            string linhaRelatorio = await GetLinhaRelatorio(pedido);
+            await System.IO.File.AppendAllLinesAsync("Relatorio.txt", new string[] { linhaRelatorio });
+        }
+
+        private static async Task<string> GetLinhaRelatorio(Pedido pedido)
+        {
+            StringBuilder sb = new StringBuilder();
+            string templatePedido =
+                    await System.IO.File.ReadAllTextAsync("TemplatePedido.txt");
+
+            string templateItemPedido =
+                await System.IO.File.ReadAllTextAsync("TemplateItemPedido.txt");
+
+            string linhaPedido =
+                string.Format(templatePedido,
+                    pedido.Id,
+                    pedido.Cadastro.Nome,
+                    pedido.Cadastro.Endereco,
+                    pedido.Cadastro.Complemento,
+                    pedido.Cadastro.Bairro,
+                    pedido.Cadastro.Municipio,
+                    pedido.Cadastro.UF,
+                    pedido.Cadastro.Telefone,
+                    pedido.Cadastro.Email,
+                    pedido.Itens.Sum(i => i.Subtotal));
+
+            sb.AppendLine(linhaPedido);
+
+            foreach (var i in pedido.Itens)
+            {
+                string linhaItemPedido =
+                    string.Format(
+                        templateItemPedido,
+                        i.Produto.Codigo,
+                        i.PrecoUnitario,
+                        i.Produto.Nome,
+                        i.Quantidade,
+                        i.Subtotal);
+
+                sb.AppendLine(linhaItemPedido);
+            }
+            sb.AppendLine($@"=============================================");
+
+            return sb.ToString();
         }
     }
 }
