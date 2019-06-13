864bf3a95b337f056525b9c7edc3cfc3b174d43d
marcelooliveira <mclricardo@gmail.com>
2019-06-13 15:30:07 -0300

    CasaDoCodigo.Identity

---
+++ depois/Item03/CasaDoCodigo.Relatorio/Controllers/ValuesController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CasaDoCodigo.RelatorioWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private static readonly List<string> Relatorio = new List<string>()
+        {
+            "primeira linha do relatório",
+            "segunda linha do relatório"
+        };
+
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in Relatorio)
+            {
+                sb.AppendLine(item);
+            }
+            return sb.ToString();
+        }
+
+        [HttpPost]
+        public void PostAsync([FromBody] string value)
+        {
+            Relatorio.Add(value);
+        }
+    }
+}
---
+++ depois/Item03/CasaDoCodigo/RelatorioHelper.cs
@@ -0,0 +1,88 @@
+﻿using CasaDoCodigo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo
+{
+    public interface IRelatorioHelper
+    {
+        Task GerarRelatorio(Pedido pedido);
+    }
+
+    public class RelatorioHelper : IRelatorioHelper
+    {
+        private const string RelatorioUri = "/api/values";
+        private readonly IHttpClientFactory httpClientFactory;
+        private readonly IConfiguration configuration;
+
+        public RelatorioHelper(IHttpClientFactory httpClientFactory,
+            IConfiguration configuration)
+        {
+            this.httpClientFactory = httpClientFactory;
+            this.configuration = configuration;
+        }
+
+        public async Task GerarRelatorio(Pedido pedido)
+        {
+            HttpClient httpClient = httpClientFactory.CreateClient();
+            string linhaRelatorio = await GetLinhaRelatorio(pedido);
+            var json = JsonConvert.SerializeObject(linhaRelatorio);
+            HttpContent httpContent = new StringContent(json, Encoding.UTF8, "application/json");
+            Uri baseUri = new Uri(configuration["CasaDoCodigo.RelatorioWebAPI"]);
+            Uri uri = new Uri(baseUri, RelatorioUri);
+            HttpResponseMessage httpResponseMessage = await httpClient.PostAsync(uri, httpContent);
+            if (!httpResponseMessage.IsSuccessStatusCode)
+            {
+                throw new ApplicationException(httpResponseMessage.ReasonPhrase);
+            }
+        }
+
+        private async Task<string> GetLinhaRelatorio(Pedido pedido)
+        {
+            StringBuilder sb = new StringBuilder();
+            string templatePedido =
+                    await System.IO.File.ReadAllTextAsync("TemplatePedido.txt");
+
+            string templateItemPedido =
+                await System.IO.File.ReadAllTextAsync("TemplateItemPedido.txt");
+
+            string linhaPedido =
+                string.Format(templatePedido,
+                    pedido.Id,
+                    pedido.Cadastro.Nome,
+                    pedido.Cadastro.Endereco,
+                    pedido.Cadastro.Complemento,
+                    pedido.Cadastro.Bairro,
+                    pedido.Cadastro.Municipio,
+                    pedido.Cadastro.UF,
+                    pedido.Cadastro.Telefone,
+                    pedido.Cadastro.Email,
+                    pedido.Itens.Sum(i => i.Subtotal));
+
+            sb.AppendLine(linhaPedido);
+
+            foreach (var i in pedido.Itens)
+            {
+                string linhaItemPedido =
+                    string.Format(
+                        templateItemPedido,
+                        i.Produto.Codigo,
+                        i.PrecoUnitario,
+                        i.Produto.Nome,
+                        i.Quantidade,
+                        i.Subtotal);
+
+                sb.AppendLine(linhaItemPedido);
+            }
+            sb.AppendLine($@"=============================================");
+
+            return sb.ToString();
+        }
+    }
+}
